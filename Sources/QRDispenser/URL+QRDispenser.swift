6e0c66d786e2b74194eeb660c8b1e5ca27affff8
Andrea Mario Lufino <andrea@andrealufino.com>
2022-08-16 21:42:45 +0200

    Created Validator class

--- Sources/QRDispenser/URL+QRDispenser.swift
+++ Sources/QRDispenser/URL+QRDispenser.swift
@@ -9,21 +9,6 @@ import Foundation
 import UIKit
 
 
-// MARK: - URL - Private
-
-fileprivate extension URL {
-    
-    var isValidNetworkURL: Bool {
-        
-        let regex = "((http|https|ftp)://)?((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+"
-        let url = self.absoluteString
-        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
-        
-        return predicate.evaluate(with: url)
-    }
-}
-
-
 // MARK: - URL - QR representation
 
 public extension URL {
@@ -31,7 +16,7 @@ public extension URL {
     /// The representation of the URL as a qr code (`UIImage` object).
     var qrRepresentation: UIImage {
         
-        guard isValidNetworkURL else {
+        guard Validator.validate(self.absoluteString, type: .url) else {
             fatalError("The URL \(self.absoluteString) is not a valid network url.")
         }
         
